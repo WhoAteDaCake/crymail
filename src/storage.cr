fdb7f9dced1ae6f71dc2895fb085bcbf14d27b8f
WhoAteDaCake <doitsimplefy@gmail.com>
2021-05-01 11:45:57 +0100

    (feat) version setup

--- src/storage.cr
+++ src/storage.cr
@@ -1,25 +1,41 @@
 require "sqlite3"
 require "db"
+require "log"
+require "./version"
 
+Logger = Log.for("Storage")
 DB_FILE="data.db"
 
 class Storage
   @conn : DB::Database 
 
   def initialize(path : String)
-    path = Path[path].join(DB_FILE).normalize
+    path = Path[path].join(DB_FILE).expand
+    File.delete(path)
     
     init = false
     if !File.exists?(path)
+      Logger.info { "Database not found assuming fresh start" }
       init = true
       File.open(path, "w") do |file| end
     end
-    puts init
+    
+    Logger.info { "Opening sqllite3 connection at #{path.to_s}"}
     @conn = DB.open "sqlite3://#{path.to_s}"
+    if init
+      setup()
+    end
   end
 
   def setup()
-    puts "hello"
+    Logger.info { "Running database setup" }
+    Logger.info { "Creating tables" }
+    @conn.exec "create table config (key text, value text)"
+
+
+    args = [] of DB::Any
+    args << Crymail::VERSION
+    @conn.exec "insert into config (key, value) values ('version', ?)", args: args
   end
 
 end
